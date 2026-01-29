d2bc2627c4000a3fdaa45e403f4ec85f82b8acd3
Sampath KVS <153347213+sampathkvs02@users.noreply.github.com>
2026-01-29 18:51:59 +0530

    project files

---
+++ src/main/java/Travel_bus_automation_testing/Travel_bus/App.java
@@ -0,0 +1,13 @@
+package Travel_bus_automation_testing.Travel_bus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
